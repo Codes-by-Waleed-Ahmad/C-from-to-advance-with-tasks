571e16f2f14e2b1a14a337ec435cef9d8943185a
Waleed Ahmad <legendwaleedwaleed@gmail.com>
2026-07-07 21:53:38 +0500

    Add files via upload

---
+++ char through cin].cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+ 	char le;
+ 	cin>>le;
+ 	cout<<"\n This will cout you the last character of the abc  which is c :"<<le;
+ 	
+ 	
+ 	
+ 	return 0;
+ }
---
+++ char to cout.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+ 	char le;
+ 	le='abc';
+ 	cout<<"\n This will cout you the last character of the abc  which is c :"<<le;
+ 	
+ 	
+ 	
+ 	return 0;
+ }
