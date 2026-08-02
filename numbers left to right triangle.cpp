e6f4dba65c91ce41fa4b5d1439f788ff1d055304
IITS/CODE/LAB <legendwaleedwaleed@gmail.com>
2026-08-02 04:43:04 -0700

    Add files via upload
    task solution

---
+++ numbers left to right triangle.cpp
@@ -0,0 +1,20 @@
+   #include<iostream>
+   using namespace std;
+   int main(){
+	int rows=5;
+	for(int i=0;i<=6;i++){
+		cout<<" ";
+		for(int j=1;j<=1+i;j++){
+		cout<<j;
+		
+		}
+		cout<<endl;
+		
+	}
+	
+	
+	
+	
+	
+	return 0;
+  }
